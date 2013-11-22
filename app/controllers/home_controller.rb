9890e4a8d873132347043efb655a8caff8252bb0
Tony Dewan <tony.dewan@gmail.com>
2013-11-22 11:39:40 -0500

    Keep people from making too many documents

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,19 @@
 class HomeController < ApplicationController
 
+  before_filter :init_session_count
+
   def index
   end
 
   def convert
+    render(status: 503) and return if session[:doc_count] > 10
     render(status: 500) and return unless params[:markdown]
 
     output = markdown.render(params[:markdown])
     response = create_pdf_in_docraptor(output)
 
     if response.code == 200
+      session[:doc_count] += 1
       send_data response, filename: "updraft_output.pdf", type: 'pdf'
     else
       render inline: response.body, status: response.code
@@ -17,6 +21,10 @@ class HomeController < ApplicationController
   end
 
   private
+
+  def init_session_count
+    session[:doc_count] ||= 0
+  end
 
   def markdown
     Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
